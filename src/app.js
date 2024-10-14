314a0f5a4a9057b9ce9eba03fe8de0e0fbf1d613
Orangel Hernandez <orangel.hernandez1999@gmail.com>
2024-10-14 19:31:59 +0000

    feat: added a card generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,53 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//variables
+const iconUp = document.querySelector("#iconUp");
+const cardValue = document.querySelector("#cardValue");
+const iconDown = document.querySelector("#iconDown");
+const numeroIconos = ["♦", "♥", "♠", "♣"];
+const numeroCarta = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
+function valorCarta() {
+  let indexNumero = Math.floor(Math.random() * numeroCarta.length);
+  return numeroCarta[indexNumero];
+}
+function valorIconos() {
+  let indexIconos = Math.floor(Math.random() * numeroIconos.length);
+  return numeroIconos[indexIconos];
+}
+function coloresIconos(icono) {
+  if (icono === "♦" || icono === "♥") {
+    iconUp.classList.add("red");
+    iconDown.classList.add("red");
+    cardValue.classList.add("red");
+  } else {
+    iconUp.classList.add("black");
+    iconDown.classList.add("black");
+    cardValue.classList.add("black");
+  }
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomCardValue = valorCarta();
+  const randomIcon = valorIconos();
+
+  cardValue.innerHTML = randomCardValue;
+  iconUp.innerHTML = randomIcon;
+  iconDown.innerHTML = randomIcon;
+
+  coloresIconos(randomIcon);
 };
